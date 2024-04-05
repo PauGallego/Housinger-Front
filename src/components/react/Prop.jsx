18f96e959b29cc984e2654d9290912f5220ff90d
david <dapapi@jviladoms.cat>
2024-04-05 08:28:59 +0200

    ARREGLADO COLORES

--- src/components/react/Prop.jsx
+++ src/components/react/Prop.jsx
@@ -23,8 +23,7 @@ const Prop = ({ url }) => {
     }
 
     let premium = propertyData.filter(property => property.premium === true);
-
-    
+    let nopremium = propertyData.filter(property => property.premium === false);
 
     if (premium.length > 0) {
         return (
@@ -45,7 +44,7 @@ const Prop = ({ url }) => {
     
                 <h2 className="font-bold text-[20px] ml-5 mt-5 mb-5 lg:ml-[95px]">Mejor valorados</h2>
                 <div className="lg:flex lg:flex-wrap m-auto lg:ml-20">
-                    {propertyData.map(property => (
+                    {nopremium.map(property => (
                         <div className="propiedad ml-[30px] lg:ml-[1%] lg:w-[400px]" key={property.id}>
                             <a href={property.propertyId}>
                                 {property.foto && <img src="public/casa1.jpg" alt="Property" className="w-[90%] h-[auto] rounded-[20px] lg:w-[100%]" />}
