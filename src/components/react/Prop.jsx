9a63a679ba4659f9e7494fb0de15b73616557334
Pau Gallego <paugf2004@gmail.com>
2024-05-13 10:05:52 +0200

    a

--- src/components/react/Prop.jsx
+++ src/components/react/Prop.jsx
@@ -2,20 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { API_BASE_URL2 } from '../../astro.config';
 import { API_BASE_URL } from '../../astro.config';
 
-const Prop = ({ urlbase, url, url2, url3, caracteristicas , startDate, endDate, location}) => {
+const Prop = ({ urlbase, url, url2, url3, caracteristicas, startDate, endDate, location }) => {
     const [propertyData, setPropertyData] = useState(null);
     const [propertyData2, setPropertyData2] = useState(null);
     const [propertyData3, setPropertyData3] = useState(null);
 
-
-    if(startDate == null){
+    if (startDate == null) {
         startDate = "";
     }
-    if(endDate == null){
+    if (endDate == null) {
         endDate = "";
     }
 
-    if(location == null){
+    if (location == null) {
         location = "";
     }
 
@@ -46,7 +45,6 @@ const Prop = ({ urlbase, url, url2, url3, caracteristicas , startDate, endDate, 
             }
         };
 
-
         const fetchData2 = async () => {
             try {
                 let response;
@@ -66,8 +64,6 @@ const Prop = ({ urlbase, url, url2, url3, caracteristicas , startDate, endDate, 
                 }
                 const data = await response.json();
                 setPropertyData2(data);
-
-
             } catch (error) {
                 console.error('Error fetching property data:', error);
             }
@@ -95,34 +91,25 @@ const Prop = ({ urlbase, url, url2, url3, caracteristicas , startDate, endDate, 
                 console.error('Error fetching property data:', error);
             }
         }
- 
+
         fetchData();
         fetchData2();
         fetchData3();
 
-
-
-
-       
-        
-
-
-    }, [urlbase, url,url2, url3, caracteristicas, startDate, endDate, location]);
+    }, [urlbase, url, url2, url3, caracteristicas, startDate, endDate, location]);
 
     let propiedadesFiltradas = [];
 
-    if (propertyData != null && propertyData2 != null && propertyData3 != null && propertyData.length > 0 && propertyData2.length > 0 && propertyData3.length > 0){
-    
+    if (propertyData != null && propertyData2 != null && propertyData3 != null && propertyData.length > 0 && propertyData2.length > 0 && propertyData3.length > 0) {
+
         propiedadesFiltradas = propertyData.filter(value =>
             propertyData2.some(obj => JSON.stringify(obj) === JSON.stringify(value)) &&
             propertyData3.some(obj => JSON.stringify(obj) === JSON.stringify(value))
         );
 
         console.log(propiedadesFiltradas);
-    
-    }
-    
 
+    }
 
     if (!propertyData || !propertyData2 || !propertyData3) {
         return (
@@ -132,47 +119,53 @@ const Prop = ({ urlbase, url, url2, url3, caracteristicas , startDate, endDate, 
         );
     }
 
-
     return (
         <div>
-    {propiedadesFiltradas && propiedadesFiltradas.length > 0 ? (
-        <div>
-            <h2 className="font-bold text-[20px] ml-5 mt-5 mb-5 lg:ml-[95px]">Recomendados</h2>
-            <div className="lg:flex lg:flex-wrap m-auto lg:ml-20">
-                {propiedadesFiltradas.filter(property => property.premium === true).map((property, index) => (
-                    <div className="propiedad ml-[30px] lg:ml-[1%] lg:w-[400px]" key={`prop_${property.id || index}_Premium`}>
-                        <a href={`${API_BASE_URL2}/user_prop?id=${property.propertyId}`}>
-                            {property.foto && <img   src={`${API_BASE_URL}/v1/fileCustomer/download/${property.foto}`}  alt="Property" className="w-[90%] h-[250px] rounded-[20px] lg:w-[100%]" />}
-                            <h2 className="lugar">{property.address}</h2>
-                            <h2 className="descripcion">{property.ownerName}</h2>
-                        </a>
+            {propiedadesFiltradas && propiedadesFiltradas.length > 0 ? (
+                <div>
+                    <h2 className="font-bold text-[20px] ml-5 mt-5 mb-5 lg:ml-[95px]">Recomendados</h2>
+                    <div className="lg:flex lg:flex-wrap m-auto lg:ml-20">
+                        {propiedadesFiltradas.filter(property => property.premium === true).map((property, index) => (
+                            <div className="propiedad ml-[30px] lg:ml-[1%] lg:w-[400px]" key={`prop_${property.id || index}_Premium`}>
+                                <a href={`${API_BASE_URL2}/user_prop?id=${property.propertyId}`}>
+                                    {property.foto ? (
+                                        <img src={`${API_BASE_URL}/v1/fileCustomer/download/${property.foto}`} alt="Property" className="w-[90%] h-[250px] rounded-[20px] lg:w-[100%]" />
+                                    ) : (
+                                        <img src={`${API_BASE_URL}/v1/fileCustomer/download/casa1.jpg`} alt="Property" className="w-[90%] h-[250px] rounded-[20px] lg:w-[100%]" />
+                                    )}
+                                    <h2 className="lugar">{property.address}</h2>
+                                    <h2 className="descripcion">{property.ownerName}</h2>
+                                </a>
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
 
-            <h2 className="font-bold text-[20px] ml-5 mt-5 mb-5 lg:ml-[95px]">Mejor valorados</h2>
-            <div className="lg:flex lg:flex-wrap m-auto lg:ml-20">
-                {propiedadesFiltradas.map((property, index) => (
-                    <div className="propiedad ml-[30px] lg:ml-[1%] lg:w-[400px]" key={`prop_${property.id || index}_NoPremium`}>
-                        <a href={`${API_BASE_URL2}/user_prop?id=${property.propertyId}`}>
-                            {property.foto && <img   src={`${API_BASE_URL}/v1/fileCustomer/download/${property.foto}`}  alt="Property" className="w-[90%] h-[250px] rounded-[20px] lg:w-[100%]" />}
-                            <h2 className="lugar">{property.address}</h2>
-                            <h2 className="descripcion">{property.ownerName}</h2>
-                        </a>
+                    <h2 className="font-bold text-[20px] ml-5 mt-5 mb-5 lg:ml-[95px]">Mejor valorados</h2>
+                    <div className="lg:flex lg:flex-wrap m-auto lg:ml-20">
+                        {propiedadesFiltradas.map((property, index) => (
+                            <div className="propiedad ml-[30px] lg:ml-[1%] lg:w-[400px]" key={`prop_${property.id || index}_NoPremium`}>
+                                <a href={`${API_BASE_URL2}/user_prop?id=${property.propertyId}`}>
+                                    {property.foto ? (
+                                        <img src={`${API_BASE_URL}/v1/fileCustomer/download/${property.foto}`} alt="Property" className="w-[90%] h-[250px] rounded-[20px] lg:w-[100%]" />
+                                    ) : (
+                                        <img src={`${API_BASE_URL}/v1/fileCustomer/download/casa1.jpg`} alt="Property" className="w-[90%] h-[250px] rounded-[20px] lg:w-[100%]" />
+                                    )}
+                                    <h2 className="lugar">{property.address}</h2>
+                                    <h2 className="descripcion">{property.ownerName}</h2>
+                                </a>
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
+                </div>
+            ) : (
+                <div className="flex justify-center mt-20 items-center flex-col">
+                    <p className='lg:text-xl md:text-lg'>No hay propiedades con estas características.</p>
+                    <img className="h-[150px] w-[150px] lg:h-[280px] lg:w-[280px] md:h-[250px] md:w-[250px]" src="../../buscar.jpg" alt="Cargando..." />
+                </div>
+            )}
         </div>
-    ) : (
-        <div className="flex justify-center mt-20 items-center flex-col">
-            <p className='lg:text-xl md:text-lg'>No hay propiedades con estas características.</p>
-            <img className="h-[150px] w-[150px] lg:h-[280px] lg:w-[280px] md:h-[250px] md:w-[250px]" src="../../buscar.jpg" alt="Cargando..." />
-        </div>
-    )}
-</div>
-
     );
-    
+
 };
 
 export default Prop;
