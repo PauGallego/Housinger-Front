cf391d9cba83675a490277dd4bc6567a624323cb
Pau Gallego <paugf2004@gmail.com>
2024-05-17 11:37:54 +0200

    a

--- src/components/react/Trade.jsx
+++ src/components/react/Trade.jsx
@@ -4,6 +4,9 @@ import { API_BASE_URL } from '../../astro.config.js';
 import { API_BASE_URL2 } from '../../astro.config.js'; 
 import '../../global.css';
 import { set } from 'date-fns';
+import Calendar2 from './CalendarioSelec.jsx'
+import Calendar3 from './CalendarioSelec2.jsx'
+import { Modal, Button, TextField } from '@mui/material';
 
 const MyComponent = () => {
 
@@ -13,10 +16,136 @@ const MyComponent = () => {
   const [reservId, setReservId] = useState(null);
   const [customerId, setCustomerId] = useState(null);
   const [data, setData] = useState({});
+  const [showModal, setShowModal] = useState(false);
+  const [showModal2, setShowModal2] = useState(false);
+  const [showModal3, setShowModal3] = useState(false);
+  const [propiedades, setPropiedades] = useState([]);
+  const [propId, setPropId] = useState(false);
 
   let userData = JSON.parse(localStorage.getItem('userData'));
 
   
+  const openModal = (id) =>{
+    setShowModal3(false);
+    setPropId(id);
+
+    setShowModal(true);
+}
+
+const closeModal = () =>{
+    setShowModal(false);
+}
+
+const openModal2 = () =>{
+    setShowModal2(true);
+}
+
+const closeModal2 = () =>{
+    setShowModal2(false);
+}
+
+const openModal3 = () =>{
+    fetchPropData();
+    localStorage.removeItem("first_date");
+    localStorage.removeItem("last_date");
+    setShowModal3(true);
+}
+
+const closeModal3 = () =>{
+    setShowModal3(false);
+}
+
+const nextModal = () =>{
+    let day = localStorage.getItem("first_date");
+    let error2 = document.getElementById("errorDiaEntrada");
+
+    if (!day) {
+        error2.innerHTML = "¡Debes seleccionar una fecha!";
+        return;
+    }
+    setShowModal(false);
+    setShowModal2(true);
+    
+}
+
+
+
+const prevModal = () =>{
+  localStorage.removeItem("first_date");
+  localStorage.removeItem("last_date");
+  setShowModal(true);
+  setShowModal2(false);
+}
+
+const fetchPropData = () =>{
+
+  const fetchData = async () => {
+      try {
+          const response = await fetch(`${API_BASE_URL}/v1/propertyUser/get/${proposerId}`);
+          const data = await response.json();
+          setPropiedades(data);
+
+          console.log(data);
+      } catch (error) {
+          console.error('Error fetching property data:', error);
+      }
+  };
+
+      
+  fetchData();
+ 
+}
+
+
+const fianlizarReserva = async () =>{
+  let last_date = localStorage.getItem("last_date");
+  let error2 = document.getElementById("errorDiaSalida");
+      
+  if (!last_date) {
+      error2.innerHTML = "¡Debes seleccionar una fecha!";
+      return;
+  }
+  
+  try {
+
+
+      const reservationData = {
+          reservationUserId:  JSON.parse(localStorage.getItem('userData')).userId,
+          reservationPropertyId: propId, 
+          dateStart:  new Date (localStorage.getItem("first_date")), 
+          dateEnd:  new Date (localStorage.getItem("last_date"))
+      };
+
+      console.log(JSON.stringify(reservationData));
+
+      const response = await fetch(`${API_BASE_URL}/v1/reservation/contraOffert?previusId=${reservId}`, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(reservationData)
+      });
+
+      if (response.ok) {
+          console.log('Reserva realizada con éxito.');
+
+          window.location.href = `${API_BASE_URL2}/chat?receiver=${customerId}`;
+   
+          closeModal2();
+      } else {
+     
+          console.error('Error al realizar la reserva:', response.statusText);
+      }
+  } catch(error) {
+     
+      console.error('Error:', error);
+  }
+  
+}
+
+
+
+
   useEffect(() => {
     const fetchData = async () => {
       const params = new URLSearchParams(window.location.search);
@@ -116,11 +245,91 @@ const MyComponent = () => {
               <p>Para las fechas</p>
                 <p>{formatDate(data.dateStart)} - {formatDate(data.dateEnd)}</p>
 
-                <button>Ver Propiedades</button>
+                <button onClick={openModal3}>Ver Propiedades</button>
                 <button onClick={ () => rechazar()}>Rechazar</button>
           </div>
         </div>
       )}
+       <Modal
+                    open={showModal3}
+                    onClose={closeModal3}
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <div className="modal-box bg-[white]">
+                        <h3 className="font-bold text-lg text-black">Porfavor, selecciona la propiedad deseada</h3>
+
+                        {propiedades.map((propiedad, index) => (
+                                <div className="flex-col mb-10 relative" key={index}>
+                                
+                                    <button onClick={ () => openModal(propiedad.propertyId)}>
+                                        <img
+                                            className="mb-5 h-[200px] w-[250px] md:h-[200px] md:w-[200px] lg:h-[200px] lg:w-[275px] rounded-[10px]"
+                                            src={propiedad.foto ? `${API_BASE_URL}/v1/fileCustomer/download/${propiedad.foto}` : `${API_BASE_URL}/v1/fileCustomer/download/casa1.jpg`}
+                                            alt=""
+                                        />
+                                        <p>{propiedad.address}</p>
+                                    </button>
+                                </div>
+                            ))}
+                        
+                         <div className="modal-action flex  items-center">
+                        <p className='text-[red] text-center' id='errorDiaSalida'></p>
+                            <button className="btn" onClick={closeModal3}>Cerrar</button>
+                        </div>
+                    </div>
+                </Modal>
+
+
+                <Modal
+                    open={showModal}
+                    onClose={closeModal}
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                  
+                  <div className="modal-box bg-[white]">
+                        <h3 className="font-bold text-lg text-black">Porfavor, selecciona las fechas deseadas</h3>
+                            <p>Dia de entrada:</p>
+                         <Calendar2 propid={propId} />
+                       
+                          <div className="modal-action flex  items-center">
+                             <p className='text-[red] text-center' id='errorDiaEntrada'></p>
+                            <button className="btn" onClick={closeModal}>Cancelar</button>
+                            <button className="btn" onClick={nextModal}>Siguiente</button>
+                           
+                        </div>
+                       
+                    </div>
+                </Modal>
+
+                <Modal
+                    open={showModal2}
+                    onClose={closeModal2}
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <div className="modal-box bg-[white]">
+                        <h3 className="font-bold text-lg text-black">Porfavor, selecciona las fechas deseadas</h3>
+                            <p>Dia de salida:</p>
+                         <Calendar3 propid={propId} />
+                        
+                         <div className="modal-action flex  items-center">
+                        <p className='text-[red] text-center' id='errorDiaSalida'></p>
+                            <button className="btn" onClick={prevModal}>Anterior</button>
+                            <button className="btn" onClick={fianlizarReserva}>Reservar</button>
+                        </div>
+                    </div>
+                </Modal>
     </div>
   );
 };
