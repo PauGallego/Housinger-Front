100feca87ed64fde4efc019c7a21fe47cb9dd001
PauGallego <paugf2004@gmail.com>
2024-05-18 20:29:33 +0200

    yess

--- src/astro.config.js
+++ src/astro.config.js
@@ -1,3 +1,3 @@
 // URL GLOABAL
-export const API_BASE_URL = 'http://localhost:8081';
+export const API_BASE_URL = 'http://housingerapi.paugallego.com';
 export const API_BASE_URL2 = 'http://localhost:4321';
