d1bbb44b1d9c078d41d135b1a9ce101e2e52329d
david <dapapi@jviladoms.cat>
2024-05-20 08:42:44 +0200

    cambios logos

--- src/astro.config.js
+++ src/astro.config.js
@@ -1,3 +1,6 @@
 // URL GLOABAL
-export const API_BASE_URL = 'http://housingerapi.paugallego.com';
+export const API_BASE_URL = 'https://housingerapi.paugallego.com';
 export const API_BASE_URL2 = 'http://localhost:4321';
+
+// export const API_BASE_URL = 'https://housingerapi.paugallego.com';
+// export const API_BASE_URL2 = 'https://housinger.paugallego.com';
