95f2a3ea75d5a599ceb47fa5fe7d8058ff994f2d
david <dapapi@jviladoms.cat>
2024-05-21 10:33:10 +0200

    sexo

--- src/astro.config.js
+++ src/astro.config.js
@@ -1,6 +1,6 @@
 // URL GLOABAL
-//export const API_BASE_URL = 'https://housingerapi.paugallego.com';
-//export const API_BASE_URL2 = 'http://localhost:4321';
+// export const API_BASE_URL = 'https://housingerapi.paugallego.com';
+// export const API_BASE_URL2 = 'http://localhost:4321';
 
- export const API_BASE_URL = 'https://housingerapi.paugallego.com';
- export const API_BASE_URL2 = 'https://housinger.paugallego.com';
+export const API_BASE_URL = 'https://housingerapi.paugallego.com';
+export const API_BASE_URL2 = 'https://housinger.paugallego.com';
